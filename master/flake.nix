7534318763f63dd79a755ca7e08c3520d67a4bec
hugosenari <hugosenari@gmail.com>
2022-03-12 02:07:17 -0300

    chore: re index hashlib

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Hash Library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-hashlib-master.flake = false;
+  inputs.src-hashlib-master.owner = "khchen";
+  inputs.src-hashlib-master.ref   = "refs/heads/master";
+  inputs.src-hashlib-master.repo  = "hashlib";
+  inputs.src-hashlib-master.type  = "github";
+  
+  inputs."nimcrypto".dir   = "nimpkgs/n/nimcrypto";
+  inputs."nimcrypto".owner = "riinr";
+  inputs."nimcrypto".ref   = "flake-pinning";
+  inputs."nimcrypto".repo  = "flake-nimble";
+  inputs."nimcrypto".type  = "github";
+  inputs."nimcrypto".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimcrypto".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-hashlib-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-hashlib-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
